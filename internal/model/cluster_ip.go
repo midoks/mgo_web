d330d2bb020c2cf75f1a73fecac8a9f18e31fb11
dami <midoks@163.com>
2026-02-07 01:16:58 +0800

    update

---
+++ internal/model/cluster_ip.go
@@ -0,0 +1,16 @@
+package model
+
+import (
+	"time"
+)
+
+type ClusterIp struct {
+	ID               int64     `json:"id" gorm:"primaryKey"`                  // unique key
+	Ip               string    `json:"ip" gorm:"unique" binding:"required"`   // ip
+	AllowPublic      bool      `json:"allow_public" binding:"required"`       // allow_public
+	AllowHealthCheck bool      `json:"allow_health_check" binding:"required"` // allow_health_check
+	Mark             string    `json:"mark"`                                  // mark
+	Status           bool      `json:"status"`                                // status
+	CreateTime       time.Time `json:"create_time"`                           // create_time
+	UpdateTime       time.Time `json:"update_time"`                           // update_time
+}
