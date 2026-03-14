c17585e62fb08a232225838a1d13e00ac6a02c7c
dami <midoks@163.com>
2026-03-14 12:07:59 +0800

    up

---
+++ internal/app/form/cluster_node_settings.go
@@ -0,0 +1,6 @@
+package form
+
+type ClusterNodeSettings struct {
+	ID   string `form:"id"`
+	Name string `form:"name"`
+}
--- internal/app/handles/backend/cluster/cluster_node_settings.go
+++ internal/app/handles/backend/cluster/cluster_node_settings.go
@@ -1,20 +1,18 @@
 package cluster
 
 import (
-	// "errors"
+	"errors"
 	// "fmt"
 	"net/http"
 	"strconv"
-
-	// "strings"
-	// "time"
+	"time"
 
 	"github.com/gin-gonic/gin"
 
 	"mgo/internal/app/common"
 	"mgo/internal/app/form"
 	"mgo/internal/db"
-	// "mgo/internal/model"
+	"mgo/internal/model"
 )
 
 func GetNodeSettingSubMenu() []form.SubSettingMenu {
@@ -57,5 +55,25 @@ func NodeSettings(c *gin.Context) {
 }
 
 func PostNodeSettings(c *gin.Context) {
+	var field form.ClusterNodeSettings
+	if err := c.ShouldBind(&field); err != nil {
+		common.ErrorResp(c, err, -1)
+		return
+	}
 
+	if field.Name == "" {
+		common.ErrorResp(c, errors.New("name cannot be empty!"), -1)
+		return
+	}
+
+	common_data := &model.ClusterNode{
+		Name:       field.Name,
+		UpdateTime: time.Now(),
+	}
+
+	if err := db.GetDb().Model(&model.Cluster{}).Where("id = ?", field.ID).Updates(common_data).Error; err != nil {
+		common.ErrorResp(c, err, -1)
+		return
+	}
+	common.SuccessResp(c)
 }
