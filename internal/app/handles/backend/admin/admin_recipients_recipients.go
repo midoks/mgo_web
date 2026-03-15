12d093bb66692d03c22ce039ca1a75f8a32fcbc1
dami <midoks@163.com>
2026-03-15 15:56:03 +0800

    update

--- internal/app/handles/backend/admin/admin_recipients_recipients.go
+++ internal/app/handles/backend/admin/admin_recipients_recipients.go
@@ -13,12 +13,12 @@ import (
 )
 
 func RecipientsRecipientsDetails(c *gin.Context) {
-	data := common.CommonVer(c)
-	data["submenu"] = GetRecipientsSubMenu()
-
 	id := c.Query("id")
 	idint, _ := strconv.ParseInt(id, 10, 64)
 	recipient_data, _ := db.GetAdminRecipientsById(idint)
+
+	data := common.CommonVer(c)
+	data["submenu"] = GetRecipientsSubMenu()
 
 	fmt.Println(recipient_data)
 	data["id"] = id
@@ -28,23 +28,32 @@ func RecipientsRecipientsDetails(c *gin.Context) {
 
 func RecipientsRecipientsUpdate(c *gin.Context) {
 	id := c.Query("id")
-	idInt, _ := strconv.ParseInt(id, 10, 64)
-	recipient_data, _ := db.GetAdminRecipientsInstancesById(idInt)
+	idint, _ := strconv.ParseInt(id, 10, 64)
+	recipient_data, _ := db.GetAdminRecipientsById(idint)
 
 	data := common.CommonVer(c)
 	data["id"] = id
 	data["Data"] = recipient_data
+
+	cluster_list, _, _ := db.GetClusterList(1, 100)
+	data["ClusterList"] = cluster_list
+
+	admin_list, _, _ := db.GetAdminList(1, 100)
+	data["AdminList"] = admin_list
+
+	recipients_list, _, _ := db.GetAdminRecipientsInstancesList(1, 100)
+	data["RecipientsList"] = recipients_list
+
 	c.HTML(http.StatusOK, "backend/admin/recipients/recipients_update.tmpl", data)
 }
 
 func RecipientsRecipientsTest(c *gin.Context) {
 	id := c.Query("id")
-	idInt, _ := strconv.ParseInt(id, 10, 64)
-	recipient_data, _ := db.GetAdminRecipientsInstancesById(idInt)
+	idint, _ := strconv.ParseInt(id, 10, 64)
+	recipient_data, _ := db.GetAdminRecipientsById(idint)
 
 	data := common.CommonVer(c)
 	data["id"] = id
 	data["Data"] = recipient_data
-
 	c.HTML(http.StatusOK, "backend/admin/recipients/recipients_test.tmpl", data)
 }
