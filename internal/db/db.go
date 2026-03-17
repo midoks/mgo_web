a2c96b413e571cd0b867f0086670103f17984d2c
dami <midoks@163.com>
2026-03-17 18:49:30 +0800

    update

--- internal/db/db.go
+++ internal/db/db.go
@@ -144,6 +144,7 @@ func Init(d *gorm.DB) {
 		new(model.SysSetting),
 		new(model.Admin),
 		new(model.AdminRecipients),
+		new(model.AdminRecipientsClusterRelated),
 		new(model.AdminMediaInstance),
 		new(model.AdminMediaGroup),
 		new(model.AdminRecipientsTasks),
--- internal/model/admin_recipients_cluster_related.go
+++ internal/model/admin_recipients_cluster_related.go
@@ -4,16 +4,11 @@ import (
 	"time"
 )
 
-type AdminRecipients struct {
+type AdminRecipientsClusterRelated struct {
 	ID          int64     `json:"id" gorm:"primaryKey"` // unique key
-	AdminID     int64     `json:"admin_id"`             // admin_id
-	MediaID     int64     `json:"media_id"`             // media_id
 	RecipientID string    `json:"recipient_id"`         // recipient_id
 	ClusterID   int64     `json:"cluster_id"`           // cluster_id
-	Mark        string    `json:"mark"`                 // mark
 	Status      bool      `json:"status"`               // status
-	TimeFrom    string    `json:"time_from"`            // time_from
-	TimeTo      string    `json:"time_to"`              // time_to
 	CreateTime  time.Time `json:"create_time"`          // create_time
 	UpdateTime  time.Time `json:"update_time"`          // update_time
 }
