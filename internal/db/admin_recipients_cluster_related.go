f49834da7ff4d92cdbb40e3c66756e44d06c54ac
dami <midoks@163.com>
2026-03-22 00:59:28 +0800

    update

--- internal/db/admin_recipients_cluster_related.go
+++ internal/db/admin_recipients_cluster_related.go
@@ -13,7 +13,7 @@ import (
 func GetAdminRecipientsClusterRelatedByRecipientID(recipient_id int64) ([]model.AdminRecipientsClusterRelated, error) {
 	recipientIDStr := fmt.Sprintf("%d", recipient_id)
 	var relations []model.AdminRecipientsClusterRelated
-	if err := db.Where("recipient_id = ?", recipientIDStr).Find(&relations).Error; err != nil {
+	if err := db.Where("recipient_id = ?", recipientIDStr).Where("status", true).Find(&relations).Error; err != nil {
 		return nil, errors.Wrap(err, "查询失败")
 	}
 	return relations, nil
@@ -23,6 +23,7 @@ func UpdateAdminRecipientsClusterRelated(tx *gorm.DB, recipient_id int64, cluste
 	if tx == nil {
 		tx = db
 	}
+
 	// 首先根据 recipient_id 查出数据库中已有的 cluster_id 列表
 	var existingRelations []model.AdminRecipientsClusterRelated
 	recipientIDStr := fmt.Sprintf("%d", recipient_id)
@@ -67,8 +68,8 @@ func UpdateAdminRecipientsClusterRelated(tx *gorm.DB, recipient_id int64, cluste
 	// 处理数据库中存在但传入列表中不存在的 cluster_id
 	for cid, relation := range existingClusterMap {
 		if !inputClusterMap[cid] {
-			// 数据库中存在但传入列表中不存在，将 status 设置为 1
-			if err := tx.Model(&relation).Update("status", true).Error; err != nil {
+			// 数据库中存在但传入列表中不存在，将 status 设置为 0
+			if err := tx.Model(&relation).Update("status", false).Error; err != nil {
 				return false, errors.Wrap(err, "更新关联状态失败")
 			}
 		}
