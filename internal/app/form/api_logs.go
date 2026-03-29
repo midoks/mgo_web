62999b58766228cc37d806031d3026ae9e372901
dami <midoks@163.com>
2026-03-29 15:38:43 +0800

    Update api_logs.go

--- internal/app/form/api_logs.go
+++ internal/app/form/api_logs.go
@@ -1,13 +1,14 @@
 package form
 
 type ApiLogs struct {
-	NodeId  string `form:"node_id"` // node_id
-	Secret  string `form:"secret"`  // secret
-	Version string `form:"version"` // 请求版本信息
+	NodeId    string `json:"node_id"`   // node_id
+	Secret    string `json:"secret"`    // secret
+	Version   string `json:"version"`   // 请求版本信息
+	Timestamp string `json:"timestamp"` // 时间戳
 
 	// type:
 	// sys		-> 系统信息
 	// node 	-> 运行信息
 	// request 	-> 应用请求
-	Type string `form:"type"`
+	Type string `json:"type"`
 }
