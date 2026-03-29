9bbf977ab9abbb6ab2293bd1fb5137d999751bab
dami <midoks@163.com>
2026-03-29 23:33:26 +0800

    Update cluster_node_logs.go

--- internal/model/cluster_node_logs.go
+++ internal/model/cluster_node_logs.go
@@ -1,16 +1,12 @@
 package model
 
-import (
-	"time"
-)
-
 type ClusterNodeLogs struct {
-	ID          int64     `json:"id" gorm:"primaryKey"`                    // unique key
-	Day         int64     `json:"day" gorm:"index"`                        // day
-	Description string    `json:"description"`                             // description
-	NodeID      int64     `json:"node_id" gorm:"index" binding:"required"` // node_id
-	Level       string    `json:"level"`                                   // level
-	Tag         string    `json:"tag"`                                     // tag
-	NodeTime    time.Time `json:"node_time"`                               // node_time
-	CreateTime  time.Time `json:"create_time"`                             // create_time
+	ID          int64  `json:"id" gorm:"primaryKey"`                    // unique key
+	Day         int64  `json:"day" gorm:"index"`                        // day
+	Description string `json:"description"`                             // description
+	NodeID      int64  `json:"node_id" gorm:"index" binding:"required"` // node_id
+	Level       string `json:"level"`                                   // level
+	Tag         string `json:"tag"`                                     // tag
+	NodeTime    int64  `json:"node_time"`                               // node_time
+	CreateTime  int64  `json:"create_time"`                             // create_time
 }
