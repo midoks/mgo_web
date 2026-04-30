7fba4dff22568812ef880adaeaf1392198eda678
dami <midoks@163.com>
2026-05-01 01:20:28 +0800

    Update install_dev.sh

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -48,7 +48,7 @@ go mod vendor
 
 # cd /usr/local/uptimepk_dev/mgo_web && go build -o mgo_web main.go 
 # cd /usr/local/uptimepk_dev/mgo_web && go build -o mgo_web main.go && ./mgo_web web
-cd $TAGRT_DIR/mgo_web && go build -o mgo main.go 
+cd $TAGRT_DIR/mgo_web && go build -o mgo_web main.go 
 systemctl daemon-reload
 
 
