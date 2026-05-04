d6b52e4b8ed0cdf099b587d3b5da789122043aca
dami <midoks@163.com>
2026-05-04 12:00:44 +0800

    Update cluster_node.go

--- internal/app/handles/backend/cluster/cluster_node.go
+++ internal/app/handles/backend/cluster/cluster_node.go
@@ -147,10 +147,86 @@ func PostCreateNode(c *gin.Context) {
 	}
 
 	if field.IpAddressesJson != "" {
-		_, err := parseClusterNodeIpArray(field.IpAddressesJson)
+		ipArray, err := parseClusterNodeIpArray(field.IpAddressesJson)
 		if err != nil {
 			common.ErrorResp(c, err, -1)
 			return
+		}
+
+		// 先全部软删除该节点的所有旧 IP 地址
+		if err := db.ClusterNodeIpaddrSoftDeleteByNodeID(nodeip.ID); err != nil {
+			common.ErrorResp(c, err, -1)
+			return
+		}
+
+		// 遍历新列表，存在就更新，不存在就创建
+		for _, ipinfo := range ipArray {
+
+			existing := db.ExistClusterNodeIpaddrByNodeIDAndIp(nodeip.ID, ipinfo.Ip)
+			if existing {
+				// 存在则更新（包含已软删除的记录）
+				updateData := map[string]interface{}{
+					"description":      ipinfo.Description,
+					"ip":               ipinfo.Ip,
+					"can_access":       ipinfo.CanAccess,
+					"can_health_check": ipinfo.CanHealthCheck,
+					"is_healthy":       true,
+					"is_on":            ipinfo.IsOn,
+					"is_up":            true,
+					"order":            1,
+					"is_deleted":       0,
+					"update_time":      time.Now().Unix(),
+				}
+				if err := db.GetDb().Unscoped().Model(&model.ClusterNodeIpaddr{}).Where("node_id = ? AND ip = ?", nodeip.ID, ipinfo.Ip).Updates(updateData).Error; err != nil {
+					common.ErrorResp(c, err, -2)
+					return
+				}
+			} else {
+				common_ip_data := &model.ClusterNodeIpaddr{
+					NodeID:         nodeip.ID,
+					Ip:             ipinfo.Ip,
+					Description:    ipinfo.Description,
+					CanAccess:      ipinfo.CanAccess,
+					CanHealthCheck: ipinfo.CanHealthCheck,
+					IsHealthy:      true,
+					IsOn:           ipinfo.IsOn,
+					IsUp:           true,
+					Order:          1,
+					IsDeleted:      0,
+				}
+
+				// 创建新数据时,先查找是否有已删除的数据
+				if delete_id, err := db.GetClusterNodeIpaddrDeletedID(); err == nil {
+					// 存在则更新（包含已软删除的记录）
+					updateData := map[string]interface{}{
+						"node_id":          nodeip.ID,
+						"description":      ipinfo.Description,
+						"ip":               ipinfo.Ip,
+						"can_access":       ipinfo.CanAccess,
+						"can_health_check": ipinfo.CanHealthCheck,
+						"is_healthy":       true,
+						"is_on":            ipinfo.IsOn,
+						"is_up":            true,
+						"order":            1,
+						"is_deleted":       0,
+						"update_time":      time.Now().Unix(),
+					}
+					if err := db.GetDb().Unscoped().Model(&model.ClusterNodeIpaddr{}).Where("id", delete_id).Updates(updateData).Error; err != nil {
+						common.ErrorResp(c, err, -2)
+						return
+					}
+
+				} else {
+
+					// 不存在则创建
+					common_ip_data.CreateTime = time.Now().Unix()
+					common_ip_data.UpdateTime = time.Now().Unix()
+					if err := db.GetDb().Create(common_ip_data).Error; err != nil {
+						common.ErrorResp(c, err, -1)
+						return
+					}
+				}
+			}
 		}
 	}
 
