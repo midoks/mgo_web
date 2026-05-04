a2528e72132d654dab42197fcffc33b369eb82a0
dami <midoks@163.com>
2026-05-04 12:03:36 +0800

    update

--- internal/app/handles/backend/cluster/cluster_node.go
+++ internal/app/handles/backend/cluster/cluster_node.go
@@ -31,6 +31,76 @@ func parseClusterNodeIpArray(ipJson string) ([]form.ClusterNodeIpAddr, error) {
 	return ipArray, nil
 }
 
+func syncClusterNodeIpAddresses(nodeID int64, ipArray []form.ClusterNodeIpAddr) error {
+	if len(ipArray) == 0 {
+		return nil
+	}
+
+	if err := db.ClusterNodeIpaddrSoftDeleteByNodeID(nodeID); err != nil {
+		return err
+	}
+
+	for _, ipinfo := range ipArray {
+		existing := db.ExistClusterNodeIpaddrByNodeIDAndIp(nodeID, ipinfo.Ip)
+		if existing {
+			updateData := map[string]interface{}{
+				"description":      ipinfo.Description,
+				"ip":               ipinfo.Ip,
+				"can_access":       ipinfo.CanAccess,
+				"can_health_check": ipinfo.CanHealthCheck,
+				"is_healthy":       true,
+				"is_on":            ipinfo.IsOn,
+				"is_up":            true,
+				"order":            1,
+				"is_deleted":       0,
+				"update_time":      time.Now().Unix(),
+			}
+			if err := db.GetDb().Unscoped().Model(&model.ClusterNodeIpaddr{}).Where("node_id = ? AND ip = ?", nodeID, ipinfo.Ip).Updates(updateData).Error; err != nil {
+				return err
+			}
+		} else {
+			common_ip_data := &model.ClusterNodeIpaddr{
+				NodeID:         nodeID,
+				Ip:             ipinfo.Ip,
+				Description:    ipinfo.Description,
+				CanAccess:      ipinfo.CanAccess,
+				CanHealthCheck: ipinfo.CanHealthCheck,
+				IsHealthy:      true,
+				IsOn:           ipinfo.IsOn,
+				IsUp:           true,
+				Order:          1,
+				IsDeleted:      0,
+			}
+
+			if deleteID, err := db.GetClusterNodeIpaddrDeletedID(); err == nil {
+				updateData := map[string]interface{}{
+					"node_id":          nodeID,
+					"description":      ipinfo.Description,
+					"ip":               ipinfo.Ip,
+					"can_access":       ipinfo.CanAccess,
+					"can_health_check": ipinfo.CanHealthCheck,
+					"is_healthy":       true,
+					"is_on":            ipinfo.IsOn,
+					"is_up":            true,
+					"order":            1,
+					"is_deleted":       0,
+					"update_time":      time.Now().Unix(),
+				}
+				if err := db.GetDb().Unscoped().Model(&model.ClusterNodeIpaddr{}).Where("id", deleteID).Updates(updateData).Error; err != nil {
+					return err
+				}
+			} else {
+				common_ip_data.CreateTime = time.Now().Unix()
+				common_ip_data.UpdateTime = time.Now().Unix()
+				if err := db.GetDb().Create(common_ip_data).Error; err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
 func GetNodeSubMenu() []form.SubMenu {
 	menu := []form.SubMenu{
 		{
@@ -153,80 +223,9 @@ func PostCreateNode(c *gin.Context) {
 			return
 		}
 
-		// 先全部软删除该节点的所有旧 IP 地址
-		if err := db.ClusterNodeIpaddrSoftDeleteByNodeID(nodeip.ID); err != nil {
+		if err := syncClusterNodeIpAddresses(nodeip.ID, ipArray); err != nil {
 			common.ErrorResp(c, err, -1)
 			return
-		}
-
-		// 遍历新列表，存在就更新，不存在就创建
-		for _, ipinfo := range ipArray {
-
-			existing := db.ExistClusterNodeIpaddrByNodeIDAndIp(nodeip.ID, ipinfo.Ip)
-			if existing {
-				// 存在则更新（包含已软删除的记录）
-				updateData := map[string]interface{}{
-					"description":      ipinfo.Description,
-					"ip":               ipinfo.Ip,
-					"can_access":       ipinfo.CanAccess,
-					"can_health_check": ipinfo.CanHealthCheck,
-					"is_healthy":       true,
-					"is_on":            ipinfo.IsOn,
-					"is_up":            true,
-					"order":            1,
-					"is_deleted":       0,
-					"update_time":      time.Now().Unix(),
-				}
-				if err := db.GetDb().Unscoped().Model(&model.ClusterNodeIpaddr{}).Where("node_id = ? AND ip = ?", nodeip.ID, ipinfo.Ip).Updates(updateData).Error; err != nil {
-					common.ErrorResp(c, err, -2)
-					return
-				}
-			} else {
-				common_ip_data := &model.ClusterNodeIpaddr{
-					NodeID:         nodeip.ID,
-					Ip:             ipinfo.Ip,
-					Description:    ipinfo.Description,
-					CanAccess:      ipinfo.CanAccess,
-					CanHealthCheck: ipinfo.CanHealthCheck,
-					IsHealthy:      true,
-					IsOn:           ipinfo.IsOn,
-					IsUp:           true,
-					Order:          1,
-					IsDeleted:      0,
-				}
-
-				// 创建新数据时,先查找是否有已删除的数据
-				if delete_id, err := db.GetClusterNodeIpaddrDeletedID(); err == nil {
-					// 存在则更新（包含已软删除的记录）
-					updateData := map[string]interface{}{
-						"node_id":          nodeip.ID,
-						"description":      ipinfo.Description,
-						"ip":               ipinfo.Ip,
-						"can_access":       ipinfo.CanAccess,
-						"can_health_check": ipinfo.CanHealthCheck,
-						"is_healthy":       true,
-						"is_on":            ipinfo.IsOn,
-						"is_up":            true,
-						"order":            1,
-						"is_deleted":       0,
-						"update_time":      time.Now().Unix(),
-					}
-					if err := db.GetDb().Unscoped().Model(&model.ClusterNodeIpaddr{}).Where("id", delete_id).Updates(updateData).Error; err != nil {
-						common.ErrorResp(c, err, -2)
-						return
-					}
-
-				} else {
-
-					// 不存在则创建
-					common_ip_data.CreateTime = time.Now().Unix()
-					common_ip_data.UpdateTime = time.Now().Unix()
-					if err := db.GetDb().Create(common_ip_data).Error; err != nil {
-						common.ErrorResp(c, err, -1)
-						return
-					}
-				}
-			}
 		}
 	}
 
--- internal/app/handles/backend/cluster/cluster_node_settings.go
+++ internal/app/handles/backend/cluster/cluster_node_settings.go
@@ -81,80 +81,9 @@ func PostNodeSettings(c *gin.Context) {
 			return
 		}
 
-		// 先全部软删除该节点的所有旧 IP 地址
-		if err := db.ClusterNodeIpaddrSoftDeleteByNodeID(field.ID); err != nil {
+		if err := syncClusterNodeIpAddresses(field.ID, ipArray); err != nil {
 			common.ErrorResp(c, err, -1)
 			return
-		}
-
-		// 遍历新列表，存在就更新，不存在就创建
-		for _, ipinfo := range ipArray {
-
-			existing := db.ExistClusterNodeIpaddrByNodeIDAndIp(field.ID, ipinfo.Ip)
-			if existing {
-				// 存在则更新（包含已软删除的记录）
-				updateData := map[string]interface{}{
-					"description":      ipinfo.Description,
-					"ip":               ipinfo.Ip,
-					"can_access":       ipinfo.CanAccess,
-					"can_health_check": ipinfo.CanHealthCheck,
-					"is_healthy":       true,
-					"is_on":            ipinfo.IsOn,
-					"is_up":            true,
-					"order":            1,
-					"is_deleted":       0,
-					"update_time":      time.Now().Unix(),
-				}
-				if err := db.GetDb().Unscoped().Model(&model.ClusterNodeIpaddr{}).Where("node_id = ? AND ip = ?", field.ID, ipinfo.Ip).Updates(updateData).Error; err != nil {
-					common.ErrorResp(c, err, -2)
-					return
-				}
-			} else {
-				common_ip_data := &model.ClusterNodeIpaddr{
-					NodeID:         field.ID,
-					Ip:             ipinfo.Ip,
-					Description:    ipinfo.Description,
-					CanAccess:      ipinfo.CanAccess,
-					CanHealthCheck: ipinfo.CanHealthCheck,
-					IsHealthy:      true,
-					IsOn:           ipinfo.IsOn,
-					IsUp:           true,
-					Order:          1,
-					IsDeleted:      0,
-				}
-
-				// 创建新数据时,先查找是否有已删除的数据
-				if delete_id, err := db.GetClusterNodeIpaddrDeletedID(); err == nil {
-					// 存在则更新（包含已软删除的记录）
-					updateData := map[string]interface{}{
-						"node_id":          field.ID,
-						"description":      ipinfo.Description,
-						"ip":               ipinfo.Ip,
-						"can_access":       ipinfo.CanAccess,
-						"can_health_check": ipinfo.CanHealthCheck,
-						"is_healthy":       true,
-						"is_on":            ipinfo.IsOn,
-						"is_up":            true,
-						"order":            1,
-						"is_deleted":       0,
-						"update_time":      time.Now().Unix(),
-					}
-					if err := db.GetDb().Unscoped().Model(&model.ClusterNodeIpaddr{}).Where("id", delete_id).Updates(updateData).Error; err != nil {
-						common.ErrorResp(c, err, -2)
-						return
-					}
-
-				} else {
-
-					// 不存在则创建
-					common_ip_data.CreateTime = time.Now().Unix()
-					common_ip_data.UpdateTime = time.Now().Unix()
-					if err := db.GetDb().Create(common_ip_data).Error; err != nil {
-						common.ErrorResp(c, err, -1)
-						return
-					}
-				}
-			}
 		}
 	}
 
@@ -165,7 +94,6 @@ func PostNodeSettings(c *gin.Context) {
 
 	common_data := &model.ClusterNode{
 		Name:            field.Name,
-		IpAddressesJson: field.IpAddressesJson,
 		UpdateTime:      time.Now().Unix(),
 	}
 
