d747ac6b60c6c04ca75e41e5d4b46a2815a2ea19
dami <midoks@163.com>
2026-05-17 14:45:07 +0800

    update

--- internal/app/handles/backend/cluster/cluster_node_logs.go
+++ internal/app/handles/backend/cluster/cluster_node_logs.go
@@ -2,6 +2,7 @@ package cluster
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 
@@ -11,10 +12,19 @@ import (
 )
 
 func NodeLogs(c *gin.Context) {
+	node_id := c.Query("node_id")
+	node_idint, _ := strconv.ParseInt(node_id, 10, 64)
+
 	data := common.CommonVer(c)
 	data["submenu"] = GetNodeSubMenu()
-	data["node_id"] = c.Query("node_id")
+	data["node_id"] = node_id
 	data["cluster_id"] = c.Query("cluster_id")
+
+	node_data, err := db.GetClusterNodeByID(node_idint)
+	if err == nil {
+		data["Data"] = node_data
+	}
+
 	c.HTML(http.StatusOK, "backend/cluster/node/logs.tmpl", data)
 }
 
